364370283e966e569e50e44a157fa17d7c8bb0d0
Kirill Makarov <kirill.makarov@rp.ru>
2021-04-20 16:11:19 +0300

    Add ESlint

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Greeting:React.FC = () => {
-  return (
-    <h1>Hello World!</h1>
-  );
-};
+const Greeting:React.FC = () => (
+  <h1>Hello World!</h1>
+);
 
 ReactDOM.render(<Greeting />, document.getElementById('root'));
