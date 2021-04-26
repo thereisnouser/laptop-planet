896a789e508c9fae4aa361fddaebbf9714894fc8
Kirill Makarov <kirill.makarov@rp.ru>
2021-04-26 11:13:42 +0300

    Change components name by naming convention

--- client/src/imports.ts
+++ client/src/imports.ts
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/app/app';
-import ShopList from './components/shopList/shopList';
-import ShopListItem from './components/shopListItem/shopListItem';
+import App from './components/App/App';
+import ShopList from './components/ShopList/ShopList';
+import ShopListItem from './components/ShopListItem/ShopListItem';
 import ShopListItemInt from './shopListItemInt';
 
-import img1 from './components/shopList/images/E210MA-GJ002T.png';
+import img1 from './components/ShopList/images/E210MA-GJ002T.png';
 
 export {
   React, ReactDOM, App, ShopList, ShopListItem, img1,
