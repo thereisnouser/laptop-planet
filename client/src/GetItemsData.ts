dfdb840f52303682acb3c2c096a3557cba98c2ad
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-11 13:53:19 +0300

    Add method for fetching data from server

--- client/src/GetItemsData.ts
+++ client/src/GetItemsData.ts
@@ -1,21 +1,16 @@
-import {
-  ShopItemInt,
-  img1,
-} from './imports';
+import { useEffect, useState } from 'react';
 
-function getItemsData() {
-  const listItems: Array<ShopItemInt> = [
-    {
-      id: 1,
-      image: img1,
-      title: 'E210MA-GJ002T',
-      price: 300,
-      description: '11.6" Ноутбук ASUS Laptop E210MA-GJ002T розовый [1366x768, TN+film, Intel Celeron N4020, 2 х 1.1 ГГц, RAM 4 ГБ, eMMC 64 ГБ, Intel UHD 600 , Wi-Fi, Windows 10 Home]',
-      rating: 4,
-    },
-  ];
+const getItemsData = () => {
+  const SERVER = 'http://localhost:5000/';
+  const [items, setItems] = useState([]);
 
-  return listItems;
+  useEffect(() => {
+    fetch(`${SERVER}api/products/`)
+      .then(res => res.json())
+      .then(res => setItems(res))
+  }, []);
+
+  return items;
 }
 
 export default getItemsData;
