6005300ff8598ffaf4c5eb026f855ce2849fe5a7
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-11 12:39:47 +0300

    Update products controller for work w/ db

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Body, Param, Get, Post, Put, Delete } from '@nestjs/common';
+import {
+  Controller, Body, Param, 
+  Get, Post, Put, Delete
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductsService } from './products.service';
 
@@ -7,32 +10,36 @@ export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
   @Post()
-  async createProduct(
-    @Body() dto: CreateProductDto) {
-    this.productsService.createProduct(dto);
+  async createProduct(@Body() dto: CreateProductDto) {
+    return this.productsService.createProduct(dto);
   }
 
   @Get()
-  async getProductsList(): Promise<Array<any>> {
-    return this.productsService.getProductsList();
+  async getProductsList() {
+    const productsList = await this.productsService.getProductsList();
+    return productsList;
   }
 
   @Get(':id')
-  async getProduct(
-    @Param('id') id: number): Promise<Array<any>> {
-    return this.productsService.getProduct(id);
+  async getProduct(@Param('id') id: number) {
+    const product = await this.productsService.getProduct(id);
+    return product;
   }
 
   @Put(':id')
   async updateProduct(
     @Param('id') id: number,
     @Body() dto: CreateProductDto) {
-    this.productsService.updateProduct(id, dto)
+    this.productsService.updateProduct(id, dto);
+
+    return `Product ${id} was updated`;
   }
 
   @Delete(':id')
   async removeProduct(
     @Param('id') id: number) {
     this.productsService.removeProduct(id);
+
+    return `Product ${id} was removed`;
   }
 }
