77168c35fc7865ef373c445fe0bf6322baefd7fb
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-11 13:51:06 +0300

    Change main ts for data sending

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const PORT = 5000;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(5000);
+  app.useStaticAssets(join(__dirname, '..', '..', 'public'));
+
+  await app.listen(PORT);
 }
 bootstrap();
