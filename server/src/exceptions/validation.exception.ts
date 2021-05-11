38bb8517330dade704f5e3666c76930d0898bafc
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-11 13:26:33 +0300

    Add exception bad request if unexpected error

---
+++ server/src/exceptions/validation.exception.ts
@@ -0,0 +1,11 @@
+import { HttpStatus } from "@nestjs/common";
+import { HttpException } from "@nestjs/common";
+
+export class ValidationException extends HttpException {
+  messages;
+
+  constructor(responce) {
+    super(responce, HttpStatus.BAD_REQUEST);
+    this.messages = responce;
+  }
+}
