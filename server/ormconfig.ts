13f0c22f64852d6dfa26acc99c800d265d9bf663
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-11 12:16:53 +0300

    Add ormconfig for db connection

---
+++ server/ormconfig.ts
@@ -0,0 +1,19 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '345YP455',
+  database: 'laptop_planet',
+  entities: ['dist/src/**/*.entity.js'],
+  migrations: [
+    'dist/src/database/migrations/*.js'
+  ],
+  cli: {
+    migrationsDir: 'src/database/migrations'
+  }
+}
+
+export default config;
