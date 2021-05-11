77ceed377f3b5872a5fc7eecb73446c75c1aa5c6
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-11 18:32:57 +0300

    Update shop item full by material ui / remove unused CSS styles

--- client/src/components/ShopItemFull/ShopItemFull.tsx
+++ client/src/components/ShopItemFull/ShopItemFull.tsx
@@ -1,5 +1,6 @@
 import {
-  React, ShopItemInt, ItemRating,
+  React, Grid, Button, Paper, Typography,
+  ShopItemInt, ItemRating,
 } from '../../imports';
 import './ShopItemFull.css';
 
@@ -9,33 +10,43 @@ const ShopItemFull: React.FC<ShopItemInt> = (props: ShopItemInt) => {
   } = props;
 
   return (
-    <div className="container">
-      <div className="shop-item-full">
-        <div className="shop-item-full__img">
-          <img src={image} alt="img-1" />
-        </div>
-        <div className="shop-item-full__info">
-          <div className="shop-item-full__header">
-            <div className="shop-item-full__title">
-              <button className="shop-item-full__title-button" onClick={() => showMoreInfoToggler(id)} type="button">
-                {title}
-              </button>
-            </div>
-            <ItemRating rating={rating} />
-          </div>
-          <div className="shop-item-full__description">
-            {description}
-          </div>
-          <div className="shop-item-full__footer">
-            <div className="shop-item-full__price">
-              $
-              {price}
-            </div>
-            <button className="shop-item-full__button" type="button">Add to cart</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Grid item key={id}>
+      <Paper elevation={4} className="shop-item-full">
+        <Grid container justify="space-around">
+          <Grid item>
+            <img className="shop-item-full__img" src={image} alt="img-1" />
+          </Grid>
+          <Grid item xs={6} container direction="column" justify="space-around">
+            <Grid item container justify="space-between" alignItems="center">
+              <Grid item>
+                <Typography className="shop-item-full__title" onClick={() => showMoreInfoToggler(id)}>
+                  {title}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography className="shop-item-full__price">
+                  $
+                  {price}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography className="shop-item-full__description">
+                {description}
+              </Typography>
+            </Grid>
+            <Grid item container justify="space-between" alignItems="center">
+              <Grid item>
+                <ItemRating rating={rating} />
+              </Grid>
+              <Grid item>
+                <Button variant="contained">Add to cart</Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Grid>
   );
 };
 
