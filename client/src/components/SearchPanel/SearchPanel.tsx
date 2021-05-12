7604bb14d59f12fef6e8ddd0d18c1d43f11f4a9d
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-12 18:47:00 +0300

    Add CSS for search panel

--- client/src/components/SearchPanel/SearchPanel.tsx
+++ client/src/components/SearchPanel/SearchPanel.tsx
@@ -1,10 +1,12 @@
 import {
   React, Paper, InputBase, IconButton, SearchIcon,
 } from '../../imports';
+import './SearchPanel.css';
 
 const SearchPanel: React.FC = () => (
-  <Paper component="form">
+  <Paper component="form" className="search-panel">
     <InputBase
+      className="search-panel__input"
       placeholder="Search..."
     />
     <IconButton type="submit" aria-label="search">
