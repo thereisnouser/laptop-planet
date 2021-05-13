32f927112005b02d137d963c943544b5c7fb3be6
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-13 14:55:49 +0300

    Add ormconfig

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,16 +4,7 @@ import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '345YP455',
-      database: 'laptop-planet',
-      entities: [],
-      synchronize: true, // only for development
-    }),
+    TypeOrmModule.forRoot(),
     ProductsModule,
   ],
 })
