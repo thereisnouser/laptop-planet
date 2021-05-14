7940795eccdfa505413679999d32ab1fd2f904c1
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-14 16:55:49 +0300

    Create interface for shop item full

--- client/src/components/ShopItemFull/ShopItemFull.tsx
+++ client/src/components/ShopItemFull/ShopItemFull.tsx
@@ -3,34 +3,42 @@ import {
 } from '../../imports';
 import './ShopItemFull.css';
 
-export const ShopItemFull: React.FC<IShopItem> = ({
-  id, image, title, price, description, rating,
-}): React.ReactElement => (
-  <div className="container">
-    <div className="shop-item-full" key={id}>
-      <div className="shop-item-full__img">
-        <img src={image} alt="img-1" />
-      </div>
-      <div className="shop-item-full__info">
-        <div className="shop-item-full__header">
-          <div className="shop-item-full__title">
-            <button className="shop-item-full__title-button" type="button">
-              {title}
-            </button>
-          </div>
-          <ItemRating rating={rating} />
+interface ShopItemFullProps {
+  item: IShopItem;
+}
+
+export const ShopItemFull: React.FC<ShopItemFullProps> = ({ item }): React.ReactElement => {
+  const {
+    id, image, title, price, description, rating,
+  } = item;
+
+  return (
+    <div className="container">
+      <div className="shop-item-full" key={id}>
+        <div className="shop-item-full__img">
+          <img src={image} alt="img-1" />
         </div>
-        <div className="shop-item-full__description">
-          {description}
-        </div>
-        <div className="shop-item-full__footer">
-          <div className="shop-item-full__price">
-            $
-            {price}
+        <div className="shop-item-full__info">
+          <div className="shop-item-full__header">
+            <div className="shop-item-full__title">
+              <button className="shop-item-full__title-button" type="button">
+                {title}
+              </button>
+            </div>
+            <ItemRating rating={rating} />
           </div>
-          <button className="shop-item-full__button" type="button">Add to cart</button>
+          <div className="shop-item-full__description">
+            {description}
+          </div>
+          <div className="shop-item-full__footer">
+            <div className="shop-item-full__price">
+              $
+              {price}
+            </div>
+            <button className="shop-item-full__button" type="button">Add to cart</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
