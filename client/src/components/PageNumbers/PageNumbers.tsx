cae50c8f6e5c81e159b3919297481cd5c123a972
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-17 17:14:35 +0300

    Add some styles for page numbers

--- client/src/components/PageNumbers/PageNumbers.tsx
+++ client/src/components/PageNumbers/PageNumbers.tsx
@@ -1,9 +1,15 @@
 import {
   React, Pagination,
 } from '../../imports';
+import './PageNumbers.css';
 
 const PageNumbers = () => (
-  <Pagination count={10} variant="outlined" shape="rounded" />
+  <Pagination
+    count={10}
+    variant="outlined"
+    shape="rounded"
+    className="page-numbers"
+  />
 );
 
 export default PageNumbers;
