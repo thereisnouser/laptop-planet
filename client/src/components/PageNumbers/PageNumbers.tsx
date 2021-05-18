ebb557121ddf1b9dd65f31c3dcf2b8993e3f3ea1
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-18 10:55:17 +0300

    Add changing url by pressing page number button

--- client/src/components/PageNumbers/PageNumbers.tsx
+++ client/src/components/PageNumbers/PageNumbers.tsx
@@ -1,15 +1,26 @@
 import {
-  React, Pagination,
+  React, Pagination, useHistory,
 } from '../../imports';
 import './PageNumbers.css';
 
-const PageNumbers = () => (
-  <Pagination
-    count={10}
-    variant="outlined"
-    shape="rounded"
-    className="page-numbers"
-  />
-);
+const PageNumbers = () => {
+  const history = useHistory();
+
+  const changePageNumber = (event: React.ChangeEvent<unknown>, value: number) => {
+    history.push({
+      search: `page=${value}`,
+    });
+  };
+
+  return (
+    <Pagination
+      onChange={changePageNumber}
+      count={10}
+      variant="outlined"
+      shape="rounded"
+      className="page-numbers"
+    />
+  );
+};
 
 export default PageNumbers;
