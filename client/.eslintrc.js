b6c7df320280775d6b7e9905a9f24ac6c4bb9e18
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-19 10:45:34 +0300

    Fix configs and remove unnecessary packages within client side

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -4,9 +4,13 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react-hooks/recommended',
+    'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -16,25 +20,111 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
-    'import/prefer-default-export': ['off'],
-    'no-plusplus': 'off',
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
-    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
-    'import/extensions': 'off',
-    'react/jsx-props-no-spreading': 'off',
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    'prettier/prettier': 'error',
+    'linebreak-style': ['error', 'unix'],
+    'arrow-body-style': [
+      'error',
+      'as-needed',
+      {
+        requireReturnForObjectLiteral: false,
       },
-    },
+    ],
+    curly: ['error', 'all'],
+    'no-implicit-coercion': ['error'],
+    'spaced-comment': ['error', 'always'],
+    eqeqeq: ['error', 'always'],
+    'prefer-template': 'error',
+    'no-useless-concat': 'error',
+    'prefer-destructuring': [
+      'error',
+      {
+        VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+        AssignmentExpression: {
+          array: false,
+          object: true,
+        },
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['../*'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/self-closing-comp': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        pathGroups: [
+          {
+            pattern: 'src/**',
+            group: 'parent',
+            position: 'after',
+          },
+        ],
+      },
+    ],
+    'import/newline-after-import': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-types': ['error'],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase'],
+      },
+      {
+        selector: ['enumMember', 'variable'],
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'parameter',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+        modifiers: ['unused'],
+      },
+      {
+        selector: 'objectLiteralProperty',
+        filter: {
+          // Regular expression for BEM classnames
+          // Source: https://medium.com/takeaway-tech/the-search-for-a-regex-to-match-hyphenated-bem-css-class-names-5f8b66cc2bd9
+          regex: '^[a-z]([a-z0-9-]+)?(__([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+){0,2}$',
+          match: true,
+        },
+        format: null,
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'typeProperty',
+        format: ['snake_case', 'camelCase'],
+      },
+    ],
   },
   overrides: [
     {
--- client/.prettierrc.js
+++ client/.prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  trailingComma: 'es5',
-  tabWidth: 2,
-  semi: true,
+  printWidth: 120,
   singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
 };
