ce3147bba436c9873cc3a2477232bff65402949d
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-24 12:18:18 +0300

    Add styles for FilterForm

--- client/src/components/FilterForm/FilterForm.tsx
+++ client/src/components/FilterForm/FilterForm.tsx
@@ -1,35 +1,45 @@
 import {
-  React, Button, FormControl, InputLabel, MenuItem, Select, TextField,
+  React, Button, FormControl, InputLabel, MenuItem, Select, TextField, Grid,
 } from '../../imports';
+import './FilterForm.css';
 
 const FilterForm = () => (
   <FormControl
     component="form"
     variant="outlined"
+    className="filter-form"
   >
-    <InputLabel id="demo-simple-select-outlined-label">Filter condition</InputLabel>
-    <Select
-      label="Filter condition"
-      labelId="demo-simple-select-outlined-label"
-      id="demo-simple-select-outlined"
-    >
-      <MenuItem value=""><em>None</em></MenuItem>
-      <MenuItem value="eq">Equal</MenuItem>
-      <MenuItem value="ne">Not equal</MenuItem>
-      <MenuItem value="gt">Greater than</MenuItem>
-      <MenuItem value="ge">Greater or equal</MenuItem>
-      <MenuItem value="lt">Less than</MenuItem>
-      <MenuItem value="le">Less or equal</MenuItem>
-    </Select>
-    <TextField
-      label="Value"
-      id="outlined-number"
-      type="number"
-      variant="outlined"
-    />
-    <Button type="submit">
-      Filter
-    </Button>
+    <Grid container spacing={2} alignItems="center">
+      <Grid item>
+        <InputLabel id="demo-simple-select-outlined-label">Filter condition</InputLabel>
+        <Select
+          label="Filter condition"
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          className="filter-form__select"
+        >
+          <MenuItem value=""><em>None</em></MenuItem>
+          <MenuItem value="eq">Equal</MenuItem>
+          <MenuItem value="ne">Not equal</MenuItem>
+          <MenuItem value="gt">Greater than</MenuItem>
+          <MenuItem value="ge">Greater or equal</MenuItem>
+          <MenuItem value="lt">Less than</MenuItem>
+          <MenuItem value="le">Less or equal</MenuItem>
+        </Select>
+      </Grid>
+      <Grid item>
+        <TextField
+          label="Value"
+          id="outlined-number"
+          type="number"
+          variant="outlined"
+          className="filter-form__input"
+        />
+      </Grid>
+      <Grid item>
+        <Button type="submit" variant="contained">Filter</Button>
+      </Grid>
+    </Grid>
   </FormControl>
 );
 
