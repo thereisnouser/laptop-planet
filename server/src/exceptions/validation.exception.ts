2837ec6761cc6582fa826c21dfcccb1e57e1a225
Kirill Makarov <kirill.makarov@rp.ru>
2021-05-26 10:11:04 +0300

    Remove custom files for validating

--- server/src/exceptions/validation.exception.ts
+++
@@ -1,9 +0,0 @@
-import { HttpStatus } from "@nestjs/common";
-import { HttpException } from "@nestjs/common";
-
-export class ValidationException extends HttpException {
-
-  constructor(responce: string[]) {
-    super(responce, HttpStatus.BAD_REQUEST);
-  }
-}
