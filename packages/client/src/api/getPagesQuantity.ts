6f982ecf871030aa28ad10ed1500331752ecbcc5
Kirill Makarov <kirill.makarov@rp.ru>
2021-06-08 12:28:33 +0300

    Add type of return value for get pages quantity method

--- packages/client/src/api/getPagesQuantity.ts
+++ packages/client/src/api/getPagesQuantity.ts
@@ -1,4 +1,4 @@
-export const getPagesQuantity = async () => {
+export const getPagesQuantity = async (): Promise<number> => {
   const response = await fetch('/api/products/count');
   return response.json();
 };
