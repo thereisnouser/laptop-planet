0fb62f9db08d64241932bccdad748ddba26c2481
Kirill Makarov <kirill.makarov@rp.ru>
2021-06-10 16:58:58 +0300

    Make users default structure

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 import { config } from 'ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), ProductsModule],
+  imports: [TypeOrmModule.forRoot(config), ProductsModule, UsersModule],
 })
 export class AppModule {}
