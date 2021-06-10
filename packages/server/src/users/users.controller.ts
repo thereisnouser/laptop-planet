e9f7251fbe278f7a5bd52d631f8bff372ab96fe7
Kirill Makarov <kirill.makarov@rp.ru>
2021-06-10 17:47:26 +0300

    Make sample CRUD for users

--- packages/server/src/users/users.controller.ts
+++ packages/server/src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  @HttpCode(204)
+  createUser(@Body() dto: CreateUserDto) {
+    return this.usersService.createUser(dto);
+  }
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsersList();
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: number) {
+    return this.usersService.getUser(Number(id));
+  }
+}
--- packages/server/src/users/users.service.ts
+++ packages/server/src/users/users.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
 
+import { CreateUserDto } from './dto/create-user.dto';
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  usersList = [
+    {
+      id: 1,
+      name: 'Markus',
+      password: 'password1',
+    },
+    {
+      id: 2,
+      name: 'John',
+      password: 'password2',
+    },
+    {
+      id: 3,
+      name: 'Jame',
+      password: 'password3',
+    },
+  ];
+
+  createUser(user: CreateUserDto) {
+    this.usersList.push(user);
+  }
+
+  getUser(id: number): CreateUserDto {
+    const index = this.usersList.findIndex(item => item.id === id);
+
+    return this.usersList[index];
+  }
+
+  getUsersList(): CreateUserDto[] {
+    return this.usersList;
+  }
+}
