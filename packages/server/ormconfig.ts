a0950986931c747ddd32d94fda8dae19b37b661d
Kirill Makarov <kirill.makarov@rp.ru>
2021-06-17 09:53:52 +0300

    Update host variable name in ormconfig

--- packages/server/ormconfig.ts
+++ packages/server/ormconfig.ts
@@ -2,7 +2,7 @@ import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConne
 
 export const config: PostgresConnectionOptions = {
   type: 'postgres',
-  host: process.env.HOST,
+  host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
